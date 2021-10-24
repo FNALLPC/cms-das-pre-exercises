ea6e2c9e64da37be241b51eede972bb74b407f84
Marguerite <mtonjes@users.noreply.github.com>
2021-10-24 18:09:49 -0500

    Update crabConfig_MC_generation.py
    Change to CMSDAS2022 name

--- code/crabConfig_MC_generation.py
+++ code/crabConfig_MC_generation.py
@@ -17,7 +17,7 @@ config.Data.unitsPerJob = 10
 NJOBS = 10  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.publication = True
-config.Data.outputDatasetTag = 'CMSDAS2021_CRAB3_MC_generation_test0'
+config.Data.outputDatasetTag = 'CMSDAS2022_CRAB3_MC_generation_test0'
 
 config.section_("Site")
 config.Site.storageSite = 'T3_US_FNALLPC'
